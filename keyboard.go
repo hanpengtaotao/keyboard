bf7ca74e4bc7210223dad1877acbdda2b92076d6
hanpengtaotao <2008hanpengtao@163.com>
2020-10-26 23:40:36 +0800

    Add files via upload

---
+++ keyboard.go
@@ -0,0 +1,23 @@
+//获取键盘输入的数值
+package keyboard
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func GetFloat() (float64, error) {
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		return 0, err
+	}
+	input = strings.TrimSpace(input)
+	number, err := strconv.ParseFloat(input, 64)
+	if err != nil {
+		return 0, err
+	}
+	return number, nil
+}
